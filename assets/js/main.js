24b7d56fb8c4d443f509b065d56177a8a1627762
suginoyuki <s_yuki_0821@icloud.com>
2024-05-17 00:08:01 +0900

    構築中

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,32 @@
-new Splide(".splide").mount();
+// new Splide(".splide").mount();
+
+var splide = new Splide('.splide', {
+  type: 'loop',
+  perPage: 3,
+  perMove: 1,
+  gap: 40,
+  pagination: false, // ページネーション非表示
+  focus: 'center',
+})
+
+splide.mount()
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // メインスライダー
-  const main = new Splide(".splide", {
-    fixedWidth: 600,
-    fixedHeight : 300,
-    perPage: 1,
-    perMpve: 1,
-    gap:40,
-    type: "slide",
-    rewind: true, // スライダーの終わりまで行ったら先頭に巻き戻す
-    pagination: false, // ページネーション非表示
-    arrows: true, // 矢印表示
-  });
+  const main = new Splide(".splide");
 
   // サムネイル
-  const thumbnails = new Splide(".thumbnail-carousel", {
-    fixedWidth: 100,
-    fixedHeight : 30,
-    type: "slide", // スライドのループさせる
-    perPage: 3, // サムネイルの表示枚数
+  const thumbnails = new Splide('.thumbnail-carousel', {
+    fixedWidth: 60,
+    fixedHeight: 35,
+    gap: 15,
+    type: 'slide', // スライドのループさせる
     pagination: false, // ページネーション非表示
     isNavigation: true, // 他のスライダーのナビゲーションとしてそれぞれのスライドをクリック可能にする
-    focus: "center", // アクティブなスライドを中央寄せ
+    focus: 'center', // アクティブなスライドを中央寄せ
     arrows: false, // 矢印非表示
-  });
+  })
   main.sync(thumbnails);
   main.mount();
   thumbnails.mount();
